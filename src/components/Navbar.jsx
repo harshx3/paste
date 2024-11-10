cdb7b4f8d4f416fd83db3365127a48c8d79426a6
Harsh Makwana <harshmakwana@Harshs-MacBook-Air.local>
2024-11-10 12:23:40 -0600

    Navbar Link Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className='flex flex-row gap-4 place-content-evenly bg-zinc-800 py-3 text-white  uppercase font-sans font-semibold'>
-            <NavLink to="/">
+            <NavLink to="/" className={({ isActive }) => isActive ? 'text-red-300' : ''} >
                 Home
             </NavLink>
-            <NavLink to="/pastes">
+            <NavLink to="/pastes" className={({ isActive }) => isActive ? 'text-red-300' : ''} >
                 Pastes
             </NavLink>
         </div>
